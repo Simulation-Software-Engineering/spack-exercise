41c256a367a96b98ecac9fcf6e807d18917f726c
Kilian Krampf <kilian@krampf.de>
2024-11-27 16:48:37 +0100

    Optional task: Add spack package options

--- package2.py
+++ package2.py
@@ -11,13 +11,13 @@ class SpackExercise(CMakePackage):
 
     homepage = "https://simulation-software-engineering.github.io/homepage/"
     url = "https://github.com/Simulation-Software-Engineering/spack-exercise/archive/refs/tags/v0.3.0.tar.gz"
-    git = "https://github.com/Simulation-Software-Engineering/spack-exercise.git"
+    git = "https://github.com/menkalian/spack-exercise.git"
 
     maintainers("menkalian")
 
     license("MIT License", checked_by="menkalian")
 
-    version("main", branch="main")
+    version("main", branch="exercise-spack-krampfkn")
     version("0.3.0", sha256="e54a4c037941d85a22fb3e6e73195df8448cf69a96aa44ef374ac518344812f0")
     version("0.2.0", sha256="3dd6b4cc0f7aff179d8e290bc3879056237ae372738a4bd7222f6450fbcdfc77")
     version("0.1.0", sha256="cac78e641cb703e3fe51956f91fe8347ac52f74ef037d8eadae5777c65a19a00")
--- yamlParser/yamlParser.cpp
+++ yamlParser/yamlParser.cpp
@@ -1,9 +1,20 @@
 #include "yamlParser.hpp"
-#include "yaml-cpp/yaml.h"
+
 #include <iostream>
 
+#ifdef WITH_YAML
+
+#include "yaml-cpp/yaml.h"
 
 void parseConfig(const std::string yamlFile){
   YAML::Node config = YAML::LoadFile(yamlFile);
   std::cout << "Version: " << config["version"].as<std::string>() << std::endl;
 }
+
+#else
+
+void parseConfig(const std::string yamlFile) {
+  std::cout << "Yaml-Parser was disabled at build time. Build package with '+yaml' to enable functionality" << std::endl;
+}
+
+#endif
