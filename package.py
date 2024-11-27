27d82798a9e2c216eaf91beea5cc91d143d3e175
Vaishnavi Wani <vaishnaviwani671@gmail.com>
2024-11-27 19:53:19 +0100

    Add main branch as version and optional dependencies using variants

--- package.py
+++ package.py
@@ -22,6 +22,8 @@
 
 from spack.package import *
 
+variant('boost', default=True, description='Enable Boost support')
+variant('yaml', default=True, description='Enable YAML support')
 
 class SpackExercise(CMakePackage):
     """An example project for learning how to package software using Spack."""
@@ -36,9 +38,11 @@ class SpackExercise(CMakePackage):
     version("0.3.0", sha256="e54a4c037941d85a22fb3e6e73195df8448cf69a96aa44ef374ac518344812f0")
     version("0.2.0", sha256="3dd6b4cc0f7aff179d8e290bc3879056237ae372738a4bd7222f6450fbcdfc77")
     version("0.1.0", sha256="cac78e641cb703e3fe51956f91fe8347ac52f74ef037d8eadae5777c65a19a00")
+    version('main', branch='main', git='https://github.com/Simulation-Software-Engineering/spack-exercise.git')
 
     depends_on("cxx", type="build")
 
     # depends_on("foo")
-    depends_on("boost@1.65.1:", when="@0.2.0:")
-    depends_on("yaml-cpp@0.7.0:", when="@0.3.0:")
+    depends_on('boost@1.65.1:', when='+boost')
+    depends_on('yaml-cpp@0.7.0:', when='+yaml')
+
