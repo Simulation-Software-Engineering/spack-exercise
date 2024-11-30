69abcfd5299b185d012cc8ae56a238231b8d8585
lingachen <kevin88415@gmail.com>
2024-11-30 03:06:36 +0100

    update package.py for optional task

--- package.py
+++ package.py
@@ -28,6 +28,7 @@ class SpackExercise(CMakePackage):
 
     homepage = "https://simulation-software-engineering.github.io/homepage/"
     url = "https://github.com/Simulation-Software-Engineering/spack-exercise/archive/refs/tags/v0.3.0.tar.gz"
+    git = "https://github.com/lingachen/spack-exercise"
 
     maintainers("lingachen")
 
@@ -36,8 +37,23 @@ class SpackExercise(CMakePackage):
     version("0.3.0", sha256="e54a4c037941d85a22fb3e6e73195df8448cf69a96aa44ef374ac518344812f0")
     version("0.2.0", sha256="3dd6b4cc0f7aff179d8e290bc3879056237ae372738a4bd7222f6450fbcdfc77")
     version("0.1.0", sha256="cac78e641cb703e3fe51956f91fe8347ac52f74ef037d8eadae5777c65a19a00")
+    version("optional-dependencies", branch="optional-dependencies-dev")
 
     depends_on("cxx", type="build")
-    depends_on("boost", when="@0.2.0:")
-    depends_on("yaml-cpp", when="@0.3.0")
+    
+    variant("boost", default=False, description="Enable Boost in CMakeLists and main.cpp")
+    variant("boost", default=True, description="Enable Boost in CMakeLists and main.cpp", when="@0.2.0:")
+    depends_on("boost", when="+boost")
+    
+    variant("yaml", default=False, description="Enable yaml-cpp in CMakeLists and main.cpp")
+    variant("yaml", default=True, description="Enable yaml-cpp in CMakeLists and main.cpp", when="@0.3.0")
+    depends_on("yaml-cpp", when="+yaml")
+
+    def cmake_args(self):
+        args = [
+            self.define_from_variant("ENABLE_BOOST", "boost"),
+            self.define_from_variant("ENABLE_YAML", "yaml"),
+        ]
+
+        return args
 
