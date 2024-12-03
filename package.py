ed47b171cb1c74a6599c6af801b8df67f0e4f1e2
Muhammad Gema Akbar <mgemaakbar@gmail.com>
2024-12-03 23:32:07 +0100

    remove comment

--- package.py
+++ package.py
@@ -1,26 +1,4 @@
-# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
-#
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install spack-exercise
-#
-# You can edit this file again by typing:
-#
-#     spack edit spack-exercise
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 from spack.package import *
-
 
 class SpackExercise(CMakePackage):
     """FIXME: Put a proper description of your package here."""
